337b64b7ea1ada9d8570c344f0a25536dc1c725f
Justus Laske <justus.laske@gmail.com>
2022-06-15 18:38:17 +0200

    Enhancement: use import

--- src/test/java/fixturefactory/TemplateNameTest.java
+++ src/test/java/fixturefactory/TemplateNameTest.java
@@ -4,10 +4,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import com.github.javafaker.Faker;
 import org.apache.commons.lang3.StringUtils;
+import org.junit.jupiter.api.Test;
 
 class TemplateNameTest {
 
-  @org.junit.jupiter.api.Test
+  @Test
   public void Should_Create_Template_Name_And_Return_Values() {
     Faker faker = new Faker();
 
@@ -18,21 +19,21 @@ class TemplateNameTest {
     assertSame(name, templateName.getName());
   }
 
-  @org.junit.jupiter.api.Test
+  @Test
   public void Should_Throw_Illegal_Argument_Exception_When_Name_Is_Null() {
     Throwable exception =
         assertThrows(IllegalArgumentException.class, () -> TemplateName.create(null));
     assertEquals("The template name must be a valid string", exception.getMessage());
   }
 
-  @org.junit.jupiter.api.Test
+  @Test
   public void Should_Throw_Illegal_Argument_Exception_When_Name_Length_Is_To_Small() {
     Throwable exception =
         assertThrows(IllegalArgumentException.class, () -> TemplateName.create("a"));
     assertEquals("The template name must have a length of at least 2", exception.getMessage());
   }
 
-  @org.junit.jupiter.api.Test
+  @Test
   public void Should_Throw_Illegal_Argument_Exception_When_Name_Length_Is_To_Long() {
     Throwable exception =
         assertThrows(
